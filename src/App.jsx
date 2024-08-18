ed764392fab092f5ca50601d7c6f5e1ddf54c12c
Ruthie Boat <ruthboatengs1@gmail.com>
2024-08-18 22:01:24 +0000

    add get user

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import PresentPage from './pages/presentPage';
 import SignIn from './pages/auth/signIn';
 import SignUp from './pages/auth/signUp';
 import FullSchool from './pages/presentPage/components/fullSchool';
+import { toast } from 'react-toastify';
+import apiGetUserDetails from './services/auth';
+import { Loader } from 'lucide-react';
+
 
 
 function App() {
@@ -26,8 +30,28 @@ function App() {
     },
     { path: "/signin", element: <SignIn /> },
     { path: "/signup", element: <SignUp /> },
-    { path: "/details", element: <FullSchool /> },
+    { path: "/details", element: <FullSchool /> 
+
+    },
+
+    {loader: async ({ params }) => {
+      const username = params.username;
+      try {
+        const response = await apiGetUserDetails(username);
+        const userProfileData = response?.data.user;
+        return userProfileData;
+      } catch (error) {
+        toast.error("An error occured");
+        return null;
+      }
+      },
+    },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
   ]);
+
   return <RouterProvider router={router} />;
 
 }
